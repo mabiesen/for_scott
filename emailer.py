0763b665a2b31fda2dc4bed85b20ac086c1698c4
Matthew Biesen <mabiesen@outlook.com>
2021-01-24 15:08:14 -0600

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -5,11 +5,11 @@ SMTP_SERVER = "smtp.gmail.com"
 SENDER_EMAIL = "upandcomming88@gmail.com"  # Enter your address
 RECEIVER_EMAIL = "upandcomming88@gmail.com"  # Enter receiver address
 
-def get_password():
-    return input("Type your email password and press enter: ")
+# Prompt will happen the moment we load this file
+PASSWORD = input("Type your email password and press enter: ")
 
-def send_email(message, password):
+def send_email(message):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
-        server.login(SENDER_EMAIL, password)
+        server.login(SENDER_EMAIL, PASSWORD)
         server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, message)
